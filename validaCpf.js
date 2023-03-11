ba059f4e3cff687b7ae09a4cf863e8a84928eed0
fregnani123 <fabianofregnani@hotmail.com>
2023-03-11 17:37:01 -0300

    estudos

--- validaCpf.js
+++ validaCpf.js
@@ -12,9 +12,11 @@ class ValidaCPF {
     eSequencia(){
         return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
     }
-    geraNovoCPF(){
+    geraNovoCpf(){
         let cpfSemDigitos = this.cpfLimpo.slice(0, -2);
         let digito1 = this.geraDigito(cpfSemDigitos);
+        let digito2 = this.geraDigito(cpfSemDigitos + digito1);
+        this.novoCPF = cpfSemDigitos + digito1 + digito2;
     }
 
     geraDigito(cpfSemDigitos){
@@ -22,22 +24,26 @@ class ValidaCPF {
         let reverso = cpfSemDigitos.length + 1;
 
         for(let stringNumerica of cpfSemDigitos){
-           
+           total += reverso * Number(stringNumerica);
+           reverso--;
         }
-
+      let digito = 11 - (total % 11);
+      return digito <= 9 ? String(digito) : '0';
     }
 
     valida() {
         if (!this.cpfLimpo) return false;
         if (typeof this.cpfLimpo !== 'string')return false;
         if (this.cpfLimpo.length !== 11) return false;
-        if (this.eSequencia()) return false;
-
-        return "CHEGUEI AQUI"
+        if(this.eSequencia())return false;
+        this.geraNovoCpf();
+        return this.novoCPF === this.cpfLimpo;
     }
 }
 
 
 let validacpf = new ValidaCPF("063.912.989-71");
 
-console.log(validacpf.valida())
+if (validacpf.valida()) {
+    console.log( "cpf valido")
+}else{console.log("cpf invalido")}
